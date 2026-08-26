96ed483c376d6a880d7c788b47eca13531c33f9d
carmelinodisalvo <carmelinodisalvo@gmail.com>
2026-08-26 16:09:08 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,12 @@
-self.addEventListener('install', (e) => {
+self.addEventListener('install', (event) => {
   self.skipWaiting();
 });
-self.addEventListener('activate', (e) => {
-  e.waitUntil(clients.claim());
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+  // Risposta standard per soddisfare i requisiti PWA
+  event.respondWith(fetch(event.request));
 });
